6b7061d8a0d6d899ce7cc75f4537ccf3a08e7c4e
Paul Woodbridge <paul.woodbridge@playmint.com>
2023-05-18 10:51:58 +0100

    SwordSmith building

---
+++ src/SwordSmith.js
@@ -0,0 +1,136 @@
+import ds from 'dawnseekers'
+
+export default function update({ selected }) {
+
+    const { tiles, seeker } = selected || {};
+    const selectedTile = tiles && tiles.length === 1 ? tiles[0] : undefined;
+    const selectedBuilding = selectedTile && selectedTile.building ? selectedTile.building : undefined;
+    const selectedEngineer = seeker;
+
+
+    const craft = () => {
+
+
+        if (!selectedEngineer) {
+            ds.log('no selected engineer');
+            return;
+        }
+        if (!selectedBuilding) {
+            ds.log('no selected building');
+            return;
+        }
+
+        //We need the IDs of the Engineer and building
+        const engineerBagOwner = selectedEngineer.id;
+        const buildingBagOwner = selectedBuilding.id;
+
+        //We use this to create a bag for the building if there isn't one to receive the items
+        const dummyBagIdIncaseToBagDoesNotExist = `0x${'00'.repeat(24)}`;
+
+
+        var bagInputSlot0 = -1;
+        var bagInputSlot1 = -1;
+        var bagOutputSlot = -1;
+
+
+        var bagInput0 = -1;
+        var bagInput1 = -1;
+        var bagOutput = -1;
+
+
+        for (var j = 0; j < selectedEngineer.bags.length; j++) {
+            for (var i = 0; i < 4; i++) {
+                if (selectedEngineer.bags[j].bag.slots[i]) {
+                    var slot = selectedEngineer.bags[j].bag.slots[i];
+
+                    if (slot.item && slot.item.id === '0x37f9b55d0000000000000000000000000000000000000003' && slot.balance >= 50) {
+                        bagInputSlot0 = i;
+                        bagInput0 = j;
+                    }
+                    else if (slot.item && slot.item.id === '0x37f9b55d0000000000000000000000000000000000000001' && slot.balance >= 10) {
+                        bagInputSlot1 = i;
+                        bagInput1 = j;
+                    }
+                    else if (!slot.item || slot.balance <= 0) {
+                        bagOutputSlot = i;
+                        bagOutput = j;
+                    }
+                }
+
+                else {
+                    bagOutputSlot = i;
+                    bagOutput = j;
+                }
+            }
+        }
+
+
+
+
+        if (bagInputSlot0 === -1 || bagInputSlot1 === -1) {
+            ds.log('Not enough crafting items');
+            return;
+        }
+        else if (bagOutputSlot === -1) {
+            ds.log('No empty slot to receive sword');
+            return;
+        }
+
+
+
+        ds.dispatch(
+            {
+                name: 'TRANSFER_ITEM_SEEKER',
+                args: [
+                    selectedEngineer.id,
+                    [engineerBagOwner, buildingBagOwner],
+                    [bagInput0, 0],
+                    [bagInputSlot0, 0],
+                    dummyBagIdIncaseToBagDoesNotExist,
+                    50,
+                ]
+            },
+            {
+                name: 'TRANSFER_ITEM_SEEKER',
+                args: [
+                    selectedEngineer.id,
+                    [engineerBagOwner, buildingBagOwner],
+                    [bagInput1, 0],
+                    [bagInputSlot1, 1],
+                    dummyBagIdIncaseToBagDoesNotExist,
+                    10,
+                ]
+            },
+            {
+                name: 'BUILDING_USE',
+                args: [selectedBuilding.id, selectedEngineer.id, ds.encodeCall(
+                    'function CRAFT_ITEM(uint64, uint64, uint8)',
+                    [selectedBuilding.bags[0].bag.key, selectedEngineer.bags[bagOutput].bag.key, bagOutputSlot]
+                )]
+            },
+        );
+
+        ds.log('SwordSmith goes brrRRRR!');
+    };
+
+
+
+    return {
+        version: 1,
+        components: [
+            {
+                type: 'building',
+                id: 'swordsmith-tutorial',
+                title: 'Tutorial SwordSmith',
+                summary: 'Sword requires 50 Iron, 10 Wood, and an empty slot for the crafted sword.',
+                content: [
+                    {
+                        id: 'default',
+                        type: 'popout',
+                        buttons: [{ text: 'Craft Sword', type: 'action', action: craft }],
+                    },
+                ],
+            },
+        ],
+    };
+}
---
+++ src/SwordSmith.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Game} from "cog/Game.sol";
+import {Node, ResourceKind, AtomKind} from "@ds/schema/Schema.sol";
+import {Actions} from "@ds/actions/Actions.sol";
+import {BuildingKind} from "@ds/ext/BuildingKind.sol";
+import {console} from "forge-std/console.sol";
+
+uint8 constant MAX_CRAFT_INPUT_ITEMS = 4; // TODO: move this into crafting rule
+
+string constant ITEM_NAME = "Sword";
+
+interface ExtensionActions {
+    function CRAFT_ITEM(
+        uint64 inBag,
+        uint64 destBag,
+        uint8 destItemSlot // empty slot
+    ) external;
+}
+
+contract SwordSmith is BuildingKind {
+    bytes24 public itemID;
+
+    constructor(Game ds) {
+        // -- Register the Item
+        bytes24[MAX_CRAFT_INPUT_ITEMS] memory inputItems;
+        uint64[MAX_CRAFT_INPUT_ITEMS] memory inputQty;
+
+        // Recipe
+        inputItems[0] = Node.Resource(ResourceKind.IRON);
+        inputQty[0] = 50;
+        inputItems[1] = Node.Resource(ResourceKind.WOOD);
+        inputQty[1] = 10;
+
+        // Boolean is the 'stackable' flag
+        ds.getDispatcher().dispatch(abi.encodeCall(Actions.REGISTER_ITEM, (inputItems, inputQty, false, ITEM_NAME)));
+
+        itemID = Node.Item(inputItems, inputQty, false, ITEM_NAME);
+    }
+
+    function use(Game ds, bytes24, /*buildingInstance*/ bytes24, /*seeker*/ bytes calldata payload) public {
+        console.log("SwordSmith::use()");
+        if (bytes4(payload) == ExtensionActions.CRAFT_ITEM.selector) {
+            console.log("SwordSmith::use(): ExtensionActions.CRAFT_ITEM");
+
+            // decode extension action
+            (uint64 inBag, uint64 destBag, uint8 destItemSlot) = abi.decode(payload[4:], (uint64, uint64, uint8));
+
+            // console.log("inBag: ", _toHexString(uint192(inBag), 24));
+            // console.log("destBag: ", _toHexString(uint192(destBag), 24));
+
+            ds.getDispatcher().dispatch(
+                abi.encodeCall(Actions.CRAFT_EQUIPABLE, (Node.Bag(inBag), itemID, Node.Bag(destBag), destItemSlot))
+            );
+        }
+    }
+}
