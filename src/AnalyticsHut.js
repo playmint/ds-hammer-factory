3f216080f1745bd502b792831520b80a8bae463d
Lucy <lucy.hill@playmint.com>
2023-05-19 12:07:42 +0100

    Pie chart now has real data

--- src/AnalyticsHut.js
+++ src/AnalyticsHut.js
@@ -4,33 +4,26 @@ export default function update(state) {
   const { world } = state;
   const seekers = (world?.tiles || []).flatMap((t) => t.seekers);
   const countSeekers = seekers.length;
-  let displayIndex = 0;
-  
-//   const toggleDisplay = () => {
-//     displayIndex = (displayIndex + 1) % 2; 
-//   };
 
-//   function getDisplayText(displayIndex) {
-//     switch (displayIndex) {
-//       case 0:
-//         return 'Show Engineer Count';
-//       case 1:
-//         return 'Show Pie Chart';
-//       default:
-//         return '';
-//     }
-//   }
-  
-//   function getDisplayHTML(displayIndex) {
-//     switch (displayIndex) {
-//       case 0:
-//         return `Total Engineers: ${countSeekers}`;
-//       case 1:
-//         return `<img src="https://chart.googleapis.com/chart?chs=250x100&chd=t:60,40&cht=p3&chf=bg,s,ffffff00&chl=Hello%7CWorld" />`;
-//       default:
-//         return '';
-//     }
-//   }
+  const values = (world?.buildings || [])
+  .flatMap((building) => building.kind?.name?.value)
+  .filter((value) => value !== null && value !== undefined);
+
+
+    const frequencies = {};
+    values.forEach((value) => {
+    if (frequencies[value]) {
+        frequencies[value] += 1;
+    } else {
+        frequencies[value] = 1;
+    }
+    });
+
+    const chartData = Object.entries(frequencies).map(([value, frequency]) => `${frequency}`).join(',');
+
+    const chartLabels = Object.keys(frequencies).join('|');
+
+    const chartURL = `https://chart.googleapis.com/chart?chs=250x100&chd=t:${chartData}&cht=p&chf=bg,s,ffffff00&chl=${chartLabels}`;
 
   return {
     version: 1,
@@ -39,7 +32,7 @@ export default function update(state) {
         id: 'my-hello-plugin',
         type: 'building',
         title: 'Analytics Hut',
-        summary: 'Click to see the total number of Engineers in the world',
+        summary: 'Pick your stat!',
         content: [
           {
             id: 'default',
@@ -60,7 +53,7 @@ export default function update(state) {
           {
             id: 'pie',
             type: 'popout',
-            html: `<img src="https://chart.googleapis.com/chart?chs=250x100&chd=t:60,40&cht=p3&chf=bg,s,ffffff00&chl=Hello%7CWorld" />`,
+            html: `<img src="${chartURL}" />`,
           },
           {
             id: 'engineercount',
